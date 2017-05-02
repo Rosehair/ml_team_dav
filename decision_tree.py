7bead3082ee10ee0ba803f535e2c6929df6ba55a
vardges <mambreyanv@gmail.com>
2017-05-02 08:22:22 +0400

    pylarm better score

--- decision_tree.py
+++ decision_tree.py
@@ -1,30 +1,34 @@
+"""implementation for desicion tree by Vardges Mmbreyan"""
 from utils import build_tree
 
 
 class DecisionTree(object):
-
+    """decision tree object"""
     def __init__(self, max_tree_depth):
         self.max_depth = max_tree_depth
 
     def fit(self, data):
+        """ft the data to tree"""
         tree = build_tree(data, max_depth=self.max_depth)
         self.tree = tree
 
-    def predict(self, X):
-        Y = []
+    def predict(self, data):
+        """predicts the data with decision tree"""
+        labels = []
 
-        def predict_x(tree, x):
-            if tree.is_leaf:
-                return list(tree.current_results)[0]
-            if x[tree.column] >= tree.value:
-                tree = tree.true_branch
-                return predict_x(tree, x)
+        def predict_x(current_tree, datum):
+            """predict the outcome of single datum"""
+            if current_tree.is_leaf:
+                return list(current_tree.current_results)[0]
+            if datum[current_tree.column] >= current_tree.value:
+                current_tree = current_tree.true_branch
+                return predict_x(current_tree, datum)
             else:
-                tree = tree.false_branch
-                return predict_x(tree, x)
+                current_tree = current_tree.false_branch
+                return predict_x(current_tree, datum)
 
-        for x in X:
+        for datum in data:
             tree = self.tree
-            Y.append(predict_x(tree, x))
+            labels.append(predict_x(tree, datum))
 
-        return Y
+        return labels
