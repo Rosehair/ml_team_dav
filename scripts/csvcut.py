b09b7977ab384957e7a6a573d49807be1df506fc
vardges <mambreyanv@gmail.com>
2017-05-07 15:15:30 +0400

    csvcut stabil, see --help for options

--- scripts/csvcut.py
+++ scripts/csvcut.py
@@ -1,5 +1,5 @@
-#!/home/vardges/anaconda3/bin/python
 from argparse import ArgumentParser
+from signal import signal, SIGPIPE, SIG_DFL
 import numpy as np
 import sys
 
@@ -8,30 +8,23 @@ DESCRIPTION = 'csvcut - Select some columns from csv streem. Could change order 
 EXAMPLES = 'example: csvcut -f 1,2 stat.txt'
 
 
-def isEqual(a, b):
-    return a == b
-
-
-def isNotEqual(a, b):
-    return a != b
-
-
-def print_row(row, column_widths, output_stream):
+def print_row(row, output_stream):
     """
-    Prints a row in human-readable format taking column widths into account
-
+    Prints a row in csv format
     :param row: row represented as a list of columns
-    :param column_widths: a list of column list widths to be used for pretty printing
     :param output_stream: a stream to pretty print the row
     """
-    output_line = '|'
-    for i, column in enumerate(row):
-        output_line += ' ' + column + ' ' * (column_widths[i] - len(column) + 1) + '|'
+    output_line = ''
+    for index, column in enumerate(row):
+        if index is 0:
+            output_line += column
+        else:
+            output_line += ',' + column
     output_line += '\n'
     output_stream.write(output_line)
 
 
-def find_features(string_f, labels, unique=False, complement=False):
+def find_features(string_f, labels, unique=False):
     """
     :param complement: if true, return indexes not in string_f
     :param unique: to take only unique fields
@@ -39,10 +32,6 @@ def find_features(string_f, labels, unique=False, complement=False):
     :param labels: first row of the data
     :return: indexes of feature ti be shown
     """
-    if complement:
-        function_compare = isNotEqual
-    else:
-        function_compare = isEqual
 
     if len(string_f) is 0:
         features = []
@@ -53,46 +42,38 @@ def find_features(string_f, labels, unique=False, complement=False):
     indexes = []
     for index, label in enumerate(labels):
         for feature in features:
-            if function_compare(label, feature):
+            if label == feature:
                 indexes.append(index)
-    print(indexes)
     return indexes
 
 
-def filter_by_column(rows, indexes):
+def filter_by_column(row, indexes):
     """
-    :param rows: original rows of file 
+    :param row: original rows of file 
     :param indexes: indexes to only show in the end
-    :return: new rows list
+    :return: new row
     """
-    new_rows = []
-    for row in rows:
-        row_add = []
-        for index in indexes:
-            row_add.append(row[index])
-        new_rows.append(row_add)
-
-    return new_rows
+    new_row = []
+    for index in indexes:
+        new_row.append(row[index])
+    return new_row
 
 
 def main():
+    signal(SIGPIPE, SIG_DFL)
     args = parse_args()
-    input_stream = open(args.file, 'r') if args.file else sys.stdin
-    num_lines = sum(1 for line in input_stream)
+
     input_stream = open(args.file, 'r') if args.file else sys.stdin
     output_stream = open(args.output_file, 'r') if args.output_file else sys.stdout
 
-    first_rows = []
-    for i in range(num_lines):
-        first_rows.append(input_stream.readline().strip().split(args.separator))
+    first_row = input_stream.readline().strip().split(args.separator)
+    index_show = find_features(args.fields, first_row, unique=args.unique)
+    print_row(filter_by_column(first_row, index_show), output_stream)
 
-    index_show = find_features(args.fields, first_rows[0], unique=args.unique, complement=args.complement)
-    first_rows = filter_by_column(first_rows, index_show)
-    columns = first_rows[0]
-    column_widths = [max([len(column) for column in [row[i] for row in first_rows]]) for i in range(len(columns))]
-
-    for row in first_rows:
-        print_row(row, column_widths, output_stream)
+    for _ in input_stream:
+        row = input_stream.readline().strip().split(args.separator)
+        row = filter_by_column(row, index_show)
+        print_row(row, output_stream)
 
     if input_stream != sys.stdin:
         input_stream.close()
@@ -106,7 +87,8 @@ def parse_args():
     parser.add_argument('-f', '--fields', type=str, help='Specify list of fields (comma separated) to cut', default='')
     parser.add_argument('-o', '--output_file', type=str, help='Output file. stdout is used by default')
     parser.add_argument('-u', '--unique', help='Remove duplicates from list of FIELDS', action='store_true')
-    parser.add_argument('-c', '--complement', help='Instead of leaving only specified columns, leave all except specified', action='store_true')
+    # parser.add_argument('-c', '--complement', help='Instead of leaving only specified columns, leave all except '
+    #                                                'specified', action='store_true')
     parser.add_argument('file', nargs='?', help='File to read input from. stdin is used by default')
 
     args = parser.parse_args()
