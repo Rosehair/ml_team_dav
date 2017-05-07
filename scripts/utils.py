b8f7b45becb6781f234c435df2055687d44c8b52
Davit Khechoyan <davkhech@gmail.com>
2017-05-07 20:32:20 +0400

    inital head, plot, utils

---
+++ scripts/utils.py
@@ -0,0 +1,33 @@
+def find_features(string_f, labels, unique=False):
+    """
+    :param complement: if true, return indexes not in string_f
+    :param unique: to take only unique fields
+    :param string_f: feature string
+    :param labels: first row of the data
+    :return: indexes of feature ti be shown
+    """
+
+    if len(string_f) is 0:
+        features = []
+    else:
+        features = string_f.split(',')
+        if unique:
+            features = list(set(features))
+    indexes = []
+    for index, label in enumerate(labels):
+        for feature in features:
+            if label == feature:
+                indexes.append(index)
+    return indexes
+
+
+def filter_by_column(row, indexes):
+    """
+    :param row: original rows of file
+    :param indexes: indexes to only show in the end
+    :return: new row
+    """
+    new_row = []
+    for index in indexes:
+        new_row.append(row[index])
+    return new_row
